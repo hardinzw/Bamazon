0905e062b7c6d97ec57d41e5c4959f68e8bd7e8a
Zachary Hardin <hardinzw@gmail.com>
2019-08-08 15:06:03 -0400

    updated table view and inquirer prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,10 @@
+//Setup
+//=========================================================================================
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -9,33 +14,113 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-//connect to database and display products
+
+//Connect to database and display table
 connection.connect(function (err) {
     if (err) throw err;
-    displayProducts();
+    storeFront();
+    displayTable();
+
 });
 
-//display products table
-function displayProducts() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log("\n" + res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock);
-    }
-  });
+//Functions
+//=========================================================================================
+//Function to display welcome
+function storeFront() {
+    console.log(
+        `
+  ***********************************************************
+                    WELCOME TO BAMAZON!
+              Thank you for shopping with us!
+  ***********************************************************
+          Here is a list of our current Merchandise.
+          See anything you like?
+  `
+    )
 };
 
-//Inquire about which ID and how many units to buy
-function purchaseProducts() {
-    inquirer
-      .prompt({
-        name: "id",
+//Function to display products for sale
+function displayTable() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        var table = new Table({
+            head: ["ID", "Product Name", "Department", "Price", "Stock Quantity"]
+        });
+
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price.toFixed(2), res[i].stock]);
+        }
+        console.log(table.toString());
+        order();
+    });
+};
+
+//Function to select ID number
+function order() {
+    inquirer.prompt({
+        name: "itemID",
         type: "input",
-        message: "Select item by ID #"
-      })
-      .then(function(answer) {
-        console.log(answer.item_id);
-        connection.query("SELECT * FROM products WHERE ?", { id: answer.item_id }, function(err, res) {
-        displayProducts();
-      });
-   });
-}
+        message: "Please select product to purchase by ID #",
+        validate: (value) => {
+            if (!isNaN(value) && (value > 0 && value <= 10)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }).then(function (answer1) {
+        var query = "SELECT * FROM products WHERE ?";
+        connection.query(query, { item_id: answer1.itemID }, function (err, res) {
+            var table = new Table({
+                head: ["ID", "Product Name", "Department", "Price", "Stock Quantity"]
+            });
+
+            for (var i = 0; i < res.length; i++) {
+                table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price.toFixed(2), res[i].stock]);
+            }
+            console.log(table.toString());
+            itemQuantity();
+        });
+    });
+};
+
+//Function to select quantity to purchase
+function itemQuantity() {
+    inquirer
+        .prompt({
+            name: "quantity",
+            type: "input",
+            message: "How many would you like to purchase?",
+            validate: function (value) {
+                if (!isNaN(value) && value > 0) {
+                    return true;
+                } else {
+                    console.log("Please enter a value greater than 0");
+                    return false;
+                }
+            }
+        }).then(function (answer) {
+            var query = "SELECT stock FROM products WHERE ?";
+            connection.query(query, { item_id: answer.quantity }, function (err, res) {
+
+                //If there is not enough stock available
+                if (res[0].stock < answer.quantity) {
+                    console.log("Insufficient quantity to process order.");
+                    inquirer.prompt({
+                        name: 'proceed',
+                        type: 'confirm',
+                        message: 'Would you like to edit your order?'
+                    }).then(function (answer) {
+                        if (answer.proceed) {
+                            itemQuantity();
+                        } else {
+                            console.log("\n\tThanks for visiting! We hope to see you again soon.");
+                            connection.end();
+                        }
+                    });
+                    //If there is enough stock available    
+                } else {
+                    console.log("processing order")
+                }
+            });
+        });
+};
